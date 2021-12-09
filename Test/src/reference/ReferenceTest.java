af7c522bdc40ea8521dd95c5a25dd836a0b79704
leo <48147598+leoluo000@users.noreply.github.com>
2021-12-09 09:54:28 +0800

    Update ReferenceTest.java

--- Test/src/reference/ReferenceTest.java
+++ Test/src/reference/ReferenceTest.java
@@ -7,7 +7,7 @@ public class ReferenceTest {
 	public static void main(String[] args) throws Exception{
 		
 // 		String str = new String("疯狂Java讲义");
-		WeakReference wr = new WeakReference(str);
+// 		WeakReference wr = new WeakReference(str);
 		str = null;
 		System.out.println(wr.get());
 		System.gc();
