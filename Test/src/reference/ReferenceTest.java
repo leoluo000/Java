09e55ac149f45f54806877f021f0d5588c443e73
leo <48147598+leoluo000@users.noreply.github.com>
2021-12-09 11:06:23 +0800

    Update ReferenceTest.java

--- Test/src/reference/ReferenceTest.java
+++ Test/src/reference/ReferenceTest.java
@@ -12,7 +12,7 @@ public class ReferenceTest {
 		System.out.println(wr.get());
 		System.gc();
 		System.runFinalization();
-		//aaaaaaa
+		//aaaaaaaaa
 		System.out.println(wr.get());
 		Object obj = wr.get();
 		if(obj == null)
